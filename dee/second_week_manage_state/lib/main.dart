b20e57346a6e4288c9dd22fdcb2c391d93fb4b04
Doy Kim <kimdee.dev@gmail.com>
2023-07-15 17:51:04 +0900

    부모가 자식위젯의 상태를 관리, TapBoxB 구현

--- dee/second_week_manage_state/lib/main.dart
+++ dee/second_week_manage_state/lib/main.dart
@@ -4,7 +4,90 @@ void main() {
   runApp(const MyApp());
 }
 
+// 2. 부모가 상태를 관리하는 코드 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo'),
+        ),
+        body: const Center(
+          child: ParentWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class ParentWidget extends StatefulWidget {
+  const ParentWidget({super.key});
+
+  @override
+  State<ParentWidget> createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext contetxt) {
+    return SizedBox(
+      child: TapBoxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+class TapBoxB extends StatelessWidget {
+  const TapBoxB({
+    super.key,
+    this.active = false,
+    required this.onChanged,
+  });
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600]
+        ),
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: const TextStyle(fontSize: 32, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/*
+// 1. 자기자신을 관리하는 위젯 코드 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -62,3 +145,4 @@ class _TapBoxAState extends State<TapBoxA> {
     );
   }
 }
+*/
