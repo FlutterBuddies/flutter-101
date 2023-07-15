8fc26f2e5b14c216adacaa74a0777c8994c2c53a
Doy Kim <kimdee.dev@gmail.com>
2023-07-15 18:13:00 +0900

    믹스매치: 부모가 자식의 상태를 일부 관리하고 자식도 자신의 상태를 관리하기 - TapBoxC 구현

--- dee/second_week_manage_state/lib/main.dart
+++ dee/second_week_manage_state/lib/main.dart
@@ -4,6 +4,126 @@ void main() {
   runApp(const MyApp());
 }
 
+// 3. 믹스-앤-매치 접근법 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo'),
+        ),
+        body: const Center(
+          child: ParentWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class ParentWidget extends StatefulWidget {
+  const ParentWidget({super.key});
+
+  @override
+  State<ParentWidget> createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: TapBoxC(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+// TapBoxC 
+class TapBoxC extends StatefulWidget {
+  const TapBoxC({
+    super.key,
+    this.active = false,
+    required this.onChanged,
+  });
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  State<TapBoxC> createState() => _TapBoxCState();
+}
+
+class _TapBoxCState extends State<TapBoxC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 탭다운에 초록보더 추가 
+    // 탭 업일 때 반대 상태로 네모가 변함
+    return GestureDetector(
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight  
+            ? Border.all (
+              color: Colors.teal[700]!,
+              width: 10,
+            )
+            : null,
+        ),
+        child: Center(
+          child:Text(widget.active ? 'Active' : 'Inactive',
+          style: const TextStyle(fontSize: 32, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
 // 2. 부모가 상태를 관리하는 코드 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -85,6 +205,8 @@ class TapBoxB extends StatelessWidget {
     );
   }
 }
+*/
+
 
 /*
 // 1. 자기자신을 관리하는 위젯 코드 
