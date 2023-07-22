3d5ce9b3170da1792b3646cd76dc7f89522563e9
techhtml <apes0123@gmail.com>
2023-07-22 15:15:00 +0900

    configure initial step

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import './pages/CupertinoStoreHomePage.dart';
+
+class CupertinoStoreApp extends StatelessWidget {
+  const CupertinoStoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+    return const CupertinoApp(
+      theme: CupertinoThemeData(brightness: Brightness.light),
+      home: CupertinoStoreHomePage(),
+    );
+  }
+}
+
