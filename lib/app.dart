287b9dd68cabb4eb49a4aeadab2fb1f4a2fb8799
techhtml <apes0123@gmail.com>
2023-07-22 15:22:43 +0900

    add tab menu

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
-import './pages/CupertinoStoreHomePage.dart';
+import './pages/home_page.dart';
 
 class CupertinoStoreApp extends StatelessWidget {
   const CupertinoStoreApp({super.key});
