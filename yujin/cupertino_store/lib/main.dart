01b3c79c45453e42532e2b2b7068d0d0f960bf97
wishJinit <wishjinit@gmail.com>
2023-07-27 23:12:51 +0900

    [Chore] .env 설정

--- yujin/cupertino_store/lib/main.dart
+++ yujin/cupertino_store/lib/main.dart
@@ -1,10 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
 import 'app.dart';
 import 'model/app_state_model.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: dotenv.get('FIREBASE_APP_KEY'),
+      appId: dotenv.get('FIREBASE_APP_ID'),
+      messagingSenderId: dotenv.get('FIREBASE_MESSAGING_SENDER_ID'),
+      projectId: dotenv.get('FIREBASE_PROJECT_ID'),
+    ),
+  );
+
   return runApp(
     ChangeNotifierProvider<AppStateModel>(
       create: (_) => AppStateModel()..loadProducts(),
