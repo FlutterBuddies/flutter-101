0e43bea94fc3442a6481e00bbb92b6c7baedab8c
이예찬 <blackmagic2@naver.com>
2023-07-29 14:20:54 +0900

    3주차 완성

--- yechan_store/lib/cupertino_store_home_page.dart
+++ yechan_store/lib/cupertino_store_home_page.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/cupertino.dart';
 
+import 'product_list_tab.dart';
+import 'search_tab.dart';
+import 'shopping_cart_tab.dart';
+
 class CupertinoStoreHomePage extends StatelessWidget {
   const CupertinoStoreHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('Cupertino Store'),
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'Products',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.shopping_cart),
+            label: 'Cart',
+          ),
+        ],
       ),
-      child: SizedBox(),
+      tabBuilder: (context, index) {
+        return switch (index) {
+          0 => CupertinoTabView(
+              builder: (context) => const CupertinoPageScaffold(
+                child: ProductListTab(),
+              ),
+            ),
+          1 => CupertinoTabView(
+              builder: (context) => const CupertinoPageScaffold(
+                child: SearchTab(),
+              ),
+            ),
+          2 => CupertinoTabView(
+              builder: (context) => const CupertinoPageScaffold(
+                child: ShoppingCartTab(),
+              ),
+            ),
+          _ => throw Exception('Invalid index $index'),
+        };
+      },
     );
   }
 }
--- yechan_store/lib/main.dart
+++ yechan_store/lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'package:yechan_store/model/app_state_model.dart';
 import 'app.dart';
 
 void main() {
-  runApp(const CupertinoStoreApp());
+  runApp(
+    ChangeNotifierProvider<AppStateModel>(
+      create: (_) => AppStateModel()..loadProducts(),
+      // A: ..연산자는 객체의 메소드를 호출하는데 연달아 호출하게 해주는 연산자. 그런데 이제 반환 값은 객체 그 자체인
+      child: const CupertinoStoreApp(),
+      // Q: 여기에 const는 왜 붙지?
+    ),
+  );
 }
