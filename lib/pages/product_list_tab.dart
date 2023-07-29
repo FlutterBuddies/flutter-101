5708dc60d6ff5b0c0e6a7d669801bc2e2556e6a6
techhtml <apes0123@gmail.com>
2023-07-29 17:16:09 +0900

    Connecting firestore database

--- lib/pages/product_list_tab.dart
+++ lib/pages/product_list_tab.dart
@@ -1,39 +1,71 @@
+import 'package:cupertino_store/model/product.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../model/app_state_model.dart';
 import '/components/product_row_item.dart';
 
-class ProductListTab extends StatelessWidget {
+class ProductListTab extends StatefulWidget {
   const ProductListTab({super.key});
 
   @override
+  State<ProductListTab> createState() => _ProductListTabState();
+}
+
+class _ProductListTabState extends State<ProductListTab> {
+  final Stream<QuerySnapshot> _productsStream =
+      FirebaseFirestore.instance.collection("products").snapshots();
+
+  @override
   Widget build(BuildContext context) {
-    return Consumer<AppStateModel>(
-      builder: (context, model, child) {
-        final products = model.getProducts();
-        return CustomScrollView(
-          semanticChildCount: products.length,
-          slivers: <Widget>[
-            const CupertinoSliverNavigationBar(
-              largeTitle: Text('Cupertino Store'),
-            ),
-            SliverSafeArea(
-              top: false,
-              minimum: const EdgeInsets.only(top: 0),
-              sliver: SliverToBoxAdapter(
-                child: CupertinoListSection(
-                  topMargin: 0,
-                  children: [
-                    for (var product in products)
-                      ProductRowItem(product: product)
-                  ],
+    return StreamBuilder<QuerySnapshot>(
+      stream: _productsStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        return Consumer<AppStateModel>(
+          builder: (context, model, child) {
+            if (snapshot.hasError) {
+              return const Text('Something went wrong');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Text("Loading");
+            }
+
+            return CustomScrollView(
+              slivers: <Widget>[
+                const CupertinoSliverNavigationBar(
+                  largeTitle: Text('Cupertino Store'),
                 ),
-              )
-            )
-          ],
+                SliverSafeArea(
+                  top: false,
+                  minimum: const EdgeInsets.only(top: 0),
+                  sliver: SliverToBoxAdapter(
+                    child: CupertinoListSection(
+                      topMargin: 0,
+                      children: snapshot.data!.docs
+                        .map((DocumentSnapshot document) {
+                          Map<String, dynamic> data =
+                              document.data() as Map<String, dynamic>;
+                          return ProductRowItem(product: Product(
+                              category: Category.values.byName(data['category']),
+                              id: data['id'],
+                              isFeatured: data['isFeatured'],
+                              name: data['name'],
+                              price: data['price'],
+                            ),
+                          );
+                      })
+                      .toList()
+                      .cast()
+                    ),
+                  )
+                )
+              ],
+            );
+          },
         );
-      },
+      }
     );
   }
 }
